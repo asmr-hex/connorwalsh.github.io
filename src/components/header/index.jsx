7e280b8dbbfc3a70942052a27aad222ff6ca1bc7
connorwalsh <c@polygon.pizza>
2016-08-28 03:03:16 -0400

    built

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -8,19 +8,19 @@ export default class Header extends Component {
 	return (
 	    <div className='header'>
 		<Logo height={170} width={170}/>
-		<HeaderText text={"GARBAGE"}
+		<HeaderText text={"{GARBAGE}"}
 			    color={"#ec95bc"}
 			    font={"Arial, Helvetica, sans-serif"}
 			    size={"160%"}
 			    href={"http://www.google.com"}
 		/>
-		<HeaderText text={"CODE"}
+		<HeaderText text={"{CODE}"}
 			    color={"#5cb2fb"}
 			    font={"Arial, Helvetica, sans-serif"}
 			    size={"160%"}
 			    href={"http://www.github.com/connorwalsh"}
 		/>
-		<HeaderText text={"LIFE SUX"}
+		<HeaderText text={"{LIFE SUX}"}
 			    color={"#f9f895"}
 			    font={"Arial, Helvetica, sans-serif"}
 			    size={"160%"}
