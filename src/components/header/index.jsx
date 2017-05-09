38ecd908ec2efb0eaa8e7181b50927f694ad344f
connorwalsh <c@polygon.pizza>
2017-05-09 18:46:43 -0400

    [cw] a breath of fresh air :)

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -4,29 +4,24 @@ import Logo from '../logo/index'
 import HeaderText from './headerText/index'
 
 export default class Header extends Component {
-    render() {
-	return (
-	    <div className='header'>
-		<Logo height={170} width={170}/>
-		<HeaderText text={"{GARBAGE}"}
-			    color={"#ec95bc"}
-			    font={"Arial, Helvetica, sans-serif"}
-			    size={"160%"}
-			    href={"http://www.google.com"}
-		/>
-		<HeaderText text={"{CODE}"}
-			    color={"#5cb2fb"}
-			    font={"Arial, Helvetica, sans-serif"}
-			    size={"160%"}
-			    href={"http://www.github.com/connorwalsh"}
-		/>
-		<HeaderText text={"{LIFE SUX}"}
-			    color={"#f9f895"}
-			    font={"Arial, Helvetica, sans-serif"}
-			    size={"160%"}
-			    href={"http://www.google.com"}
-		/>
-	    </div>
-	)
-    }
+  render() {
+    return (
+      <div className='header'>
+        <h1>UNDER CONSTRUCTION :(</h1>
+	<Logo height={170} width={170}/>
+	<HeaderText text={"{OLD SITE}"}
+		    color={"#ec95bc"}
+		    font={"Arial, Helvetica, sans-serif"}
+		    size={"160%"}
+		    href={"http://connorwalsh.github.io/old"}
+		    />
+	<HeaderText text={"{CODE}"}
+		    color={"#5cb2fb"}
+		    font={"Arial, Helvetica, sans-serif"}
+		    size={"160%"}
+		    href={"http://www.github.com/connorwalsh"}
+		    />
+      </div>
+    )
+  }
 }
